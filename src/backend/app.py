63be1b881d00c090e739ca8d42a2a155b95d6dba
Johannes Veit <dr.kugelmehl@googlemail.com>
2022-01-28 00:24:27 +0100

    Formatting

--- src/backend/app.py
+++ src/backend/app.py
@@ -3,23 +3,28 @@ from flask_cors import CORS
 
 from .solver import WordleSolver
 
+
 app = Flask(__name__)
 CORS(app)
 
 
 @app.route("/")
 def suggestions():
-    n = request.args.get("n", 5, int)
-    n_suggestions = request.args.get("n_suggestions", 10, int)
     game_str = request.args.get("game", "", str)
     game = [move.split(":") for move in game_str.split(",")] if game_str else []
+    n_suggestions = request.args.get("n_suggestions", 10, int)
+    n = request.args.get("n", 5, int)
     dict_file = "wordle_answers.txt" if n == 5 else "words.txt"
+
     solver = WordleSolver(dict_file, n)
+
     for guess, response in game:
         solver.update_knowledge(guess, response)
-    all_suggestions = solver.suggestions()
+
+    suggestions = solver.suggestions()
+
     return dict(
+        valid_words_left=len(suggestions),
         n_suggestions=n_suggestions,
-        suggestions=all_suggestions[:n_suggestions],
-        valid_words_left=len(all_suggestions)
+        suggestions=suggestions[:n_suggestions],
     )
